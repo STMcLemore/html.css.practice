b04952f6d08ca5d271a057f634056eb8df23fdd2
STMcLemore <stmclemore@hotmail.com>
2026-03-11 21:42:45 -0400

    first commit

---
+++ code.js
@@ -0,0 +1,10 @@
+let age;
+age = 32;
+let favoriteFood = "Pizza";
+let firstName = "Stephen";
+
+console.log(firstName.charAt(3));
+
+// document.getElementById("p1").textContent = `My name is ${firstName}.`;
+// document.getElementById("p2").textContent = `I am ${age} years old.`;
+// document.getElementById("p3").textContent = `My favorite food is ${favoriteFood}.`;
