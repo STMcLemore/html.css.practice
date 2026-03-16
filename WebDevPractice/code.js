2f898503979d1200e9b35ace07daf04e7b4bb25d
STMcLemore <stmclemore@hotmail.com>
2026-03-15 22:06:28 -0400

    added addition paragraphs, link buttons, added box-shadows

--- WebDevPractice/code.js
+++ WebDevPractice/code.js
@@ -2,9 +2,21 @@ let age;
 age = 32;
 let favoriteFood = "Pizza";
 let firstName = "Stephen";
+let interest = "Web Development";
+let school = "Code Louisville";
+let city = "Louisville";
+let LinkedIn = "https://www.linkedin.com/in/stephen-mclemore-dev217/";
+let GitHub = "https://github.com/STMcLemore";
+
 
 console.log(firstName.charAt(3));
 
  document.getElementById("p1").textContent = `My name is ${firstName}.`;
  document.getElementById("p2").textContent = `I am ${age} years old.`;
  document.getElementById("p3").textContent = `My favorite food is ${favoriteFood}.`;
+ document.getElementById("p4").textContent = `I am interested in ${interest}.`;
+ document.getElementById("p5").textContent = `I attend ${school}.`;
+ document.getElementById("p6").textContent = `I live in ${city}.`;
+ document.getElementById("l1").href = LinkedIn;
+ document.getElementById("l2").href = GitHub;
+
